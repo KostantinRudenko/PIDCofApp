42101748cf3e382be40532cfc96a5700dd0a8244
KostantinRudenko <rudenkokostya20@gmail.com>
2024-10-10 21:22:54 +0300

    y = ax^2 +bx + c

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,110 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			button1 = new Button();
+			textBoxA = new TextBox();
+			textBoxB = new TextBox();
+			textBoxC = new TextBox();
+			label1 = new Label();
+			label2 = new Label();
+			label3 = new Label();
+			mainPanel = new Panel();
 			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(397, 224);
+			button1.Name = "button1";
+			button1.Size = new Size(75, 25);
+			button1.TabIndex = 0;
+			button1.Text = "Paint";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// textBoxA
+			// 
+			textBoxA.Location = new Point(370, 10);
+			textBoxA.Name = "textBoxA";
+			textBoxA.Size = new Size(100, 23);
+			textBoxA.TabIndex = 1;
+			// 
+			// textBoxB
+			// 
+			textBoxB.Location = new Point(370, 40);
+			textBoxB.Name = "textBoxB";
+			textBoxB.Size = new Size(100, 23);
+			textBoxB.TabIndex = 2;
+			// 
+			// textBoxC
+			// 
+			textBoxC.Location = new Point(370, 70);
+			textBoxC.Name = "textBoxC";
+			textBoxC.Size = new Size(100, 23);
+			textBoxC.TabIndex = 3;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(350, 10);
+			label1.Name = "label1";
+			label1.Size = new Size(16, 15);
+			label1.TabIndex = 4;
+			label1.Text = "a:";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(350, 40);
+			label2.Name = "label2";
+			label2.Size = new Size(17, 15);
+			label2.TabIndex = 5;
+			label2.Text = "b:";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(350, 70);
+			label3.Name = "label3";
+			label3.Size = new Size(16, 15);
+			label3.TabIndex = 6;
+			label3.Text = "c:";
+			// 
+			// mainPanel
+			// 
+			mainPanel.Location = new Point(12, 12);
+			mainPanel.Name = "mainPanel";
+			mainPanel.Size = new Size(332, 228);
+			mainPanel.TabIndex = 7;
+			mainPanel.Paint += mainPanel_Paint;
 			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(384, 261);
+			ClientSize = new Size(484, 261);
+			Controls.Add(mainPanel);
+			Controls.Add(label3);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(textBoxC);
+			Controls.Add(textBoxB);
+			Controls.Add(textBoxA);
+			Controls.Add(button1);
 			Name = "Form1";
 			Text = "Form1";
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
 
-		private Panel panel1;
+		private Button button1;
+		private TextBox textBoxA;
+		private TextBox textBoxB;
+		private TextBox textBoxC;
+		private Label label1;
+		private Label label2;
+		private Label label3;
+		private Panel mainPanel;
 	}
 }
--- Form1.cs
+++ Form1.cs
@@ -2,32 +2,60 @@ namespace PIDCofApp
 {
 	public partial class Form1 : Form
 	{
+		private int N = 40;
+		private bool shouldDraw = false;
+		private Point[] points;
+
+
 		public Form1()
 		{
+			points = new Point[N+1];
 			InitializeComponent();
-			this.Paint += new PaintEventHandler(PaintParabola);
+			this.Paint += new PaintEventHandler(mainPanel_Paint);
 		}
 
-		private void PaintParabola(object sender, PaintEventArgs e)
+		private void button1_Click(object sender, EventArgs pe)
 		{
-			Pen pen = new Pen(Color.Black, 2);
-			
-			int num = 15;
-
-			Point[] points = new Point[num*2+1];
-			int centerX = this.ClientSize.Width/2;
-			int centerY = this.ClientSize.Height;
-
-
-			for (int x = -num; x <= num; x++)
-			{
-				int y = x * x;
-				points[x + num] = new Point(centerX + x, centerY - y);
-			}
-			e.Graphics.DrawCurve(pen, points);
-
-			pen.Dispose();
+			shouldDraw = true;
+			mainPanel.Invalidate();
 		}
 
+		private void mainPanel_Paint(object sender, PaintEventArgs e)
+		{
+			if (shouldDraw)
+			{
+				int xRange = N / 2;
+
+				string strA, strB, strC;
+				strA = textBoxA.Text;
+				strB = textBoxB.Text;
+				strC = textBoxC.Text;
+
+				if (strA != "" &&
+					strB != "" &&
+					strC != "")
+				{
+
+					int a = int.Parse(strA);
+					int b = int.Parse(strB);
+					int c = int.Parse(strC);
+
+					for (int x = -xRange; x <= xRange; x++)
+					{
+						int y = a * x * x + b * x + c;
+						points[x + xRange] = new Point(
+							x + mainPanel.Width / 2,
+							mainPanel.Height - y);
+					}
+				}
+
+				Pen pen = new Pen(Color.Black, 2);
+				using (Graphics g = mainPanel.CreateGraphics())
+				{
+					g.DrawCurve(pen, points);
+				}
+				pen.Dispose();
+			}
+		}
 	}
 }
